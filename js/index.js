4feed503a52364c5da732ca4e832d99506f0863a
chuerfano <chuerfano@grupodot.com>
2018-10-01 17:49:02 -0500

    Woow

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var html_head = '';
+var html_body = '';
+
+html_head += '<!DOCTYPE html>\n' +
+'<html lang="en">\n' +
+'<head>\n' +
+'    <meta charset="UTF-8">\n' +
+'    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
+'    <meta http-equiv="X-UA-Compatible" content="ie=edge">\n' +
+'    <title>Document</title>\n' +
+'    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">\n' +
+'</head>\n' +
+'<body>\n' +
+'    <div class="container">\n' +
+'    <!-- Content here -->\n' +
+'        <div class="form-group">\n' +
+'          <label class="control-label">Label</label>\n' +
+'          <input type="text" name="" placeholder="" class="form-control"/>\n' +
+'        </div>\n\n';
+ 
+html_body += '    </div>\n' +
+'</body>\n' +
+'</html>\n';
